cdc8d24a1063be9b71bb2f3a95e299714a625b7c
cooperyen <dknick081@gmail.com>
2025-01-03 00:27:24 +0800

    updata:layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,46 @@
-import { fontFamily as _fontFamily } from "tailwindcss/defaultTheme";
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
-export const content = [
-  "./components/**/*.{js,vue,ts}",
-  "./layouts/**/*.vue",
-  "./pages/**/*.vue",
-  "./plugins/**/*.{js,ts}",
-  "./app.vue",
-  "./error.vue",
-];
-export const theme = {
-  extend: {
-    fontFamily: {
-      sans: ['"Noto Sans TC"', ..._fontFamily.sans],
-    },
-    keyframes: {
-      "bounce-stand": {
-        "0%": { top: "-50px" },
-        "100%": { top: "0px" },
+module.exports = {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./app.vue",
+    "./error.vue",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['"Noto Sans TC"', ...defaultTheme.fontFamily.sans],
       },
-      "bounce-stand-back": {
-        "0%": { top: "50px" },
-        "100%": { top: "0px" },
+      keyframes: {
+        "bounce-stand": {
+          "0%": { top: "-50px" },
+          "100%": { top: "0px" },
+        },
+        "bounce-stand-back": {
+          "0%": { top: "50px" },
+          "100%": { top: "0px" },
+        },
+        "top-to-bottom": {
+          "0%": { transform: "-500px" },
+          "100%": { translateY: "0px" },
+        },
       },
-      "top-to-bottom": {
-        "0%": { transform: "-500px" },
-        "100%": { translateY: "0px" },
+      animation: {
+        "bounce-stand": "bounce-stand 0.4s ease-in forwards",
+        "bounce-stand-back": "bounce-stand-back 0.4s ease-in forwards",
+        "top-to-bottom": "top-to-bottom 0.4s ease-in forwards",
+      },
+      colors: {
+        background: "rgb(253,253,253)",
+        "main-blue": "#263d56",
+        "main-gray": "#ededed",
+        "light-gray": "rgb(235,235,235)",
+        tag: "rgb(228 238 243)",
+        mian_color: "#0891b2",
       },
     },
     animation: {
