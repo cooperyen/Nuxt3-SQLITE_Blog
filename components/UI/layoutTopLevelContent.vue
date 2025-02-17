f842e2d643d2740e63b410558ac1fd2eb14791ac
cooperyne <dknick081@gmail.com>
2025-02-18 00:38:11 +0800

    fix:pagination

--- components/UI/layoutTopLevelContent.vue
+++ components/UI/layoutTopLevelContent.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="fixed bg-[rgba(0,0,0,0.20)] z-50 left-0 top-0 w-full h-full">
     <div
-      class="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 max-w-96 w-full text-center">
+      class="bg-white rounded-md absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 max-w-96 w-full text-center">
       <div class=" p-5 mx-2 rounded-md">
         <slot></slot>
       </div>
